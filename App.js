8240a02bb7047e58ad2137c519275112567df00e
MounikaBompelly <141317409+MounikaBompelly@users.noreply.github.com>
2023-08-22 10:47:52 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css';
+import Login from "./Components/login";
+function App() {
+  return (
+    <div className="App">
+    <h1>Simple Login Page</h1>
+    <Login />
+    </div>
+  );
+}
+
+export default App;
